3fe90867fcca86809ebad6cc0ecf6e077913c2ba
Andrew Kononenko <wastardy.k@gmail.com>
2025-06-28 13:20:18 +0300

    feat(Header): enhance header styling with Tailwind CSS classes

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -1,8 +1,8 @@
 export function Header({ image }) {
   return (
-    <header>
+    <header className="flex flex-col items-center mt-8 mb-24">
       <h1>Vite + React</h1>
-      <img src={image} alt="" />
+      <img src={image} alt="" className="mb-8 w-50 h-50" />
       <p>Welcome to your Vite + React application!</p>
       <p className="text-gray-100 text-sm leading-relaxed">
         React is a JavaScript library for building user interfaces.
